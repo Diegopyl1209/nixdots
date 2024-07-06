cb295e3f29c5ffbb2cfa80e4d5be3a81892b00c3
Diegopyl1209 <80992641+Diegopyl1209@users.noreply.github.com>
2024-07-06 13:29:25 -0400

    update nix hardware

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,2 +1,7 @@
-{pkgs, ...}: {
+{pkgs, inputs, ...}: {
+  imports = [
+    inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+    inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
+  ];
 }
--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -1,8 +1,15 @@
 {
   username,
   pkgs,
+  inputs,
   ...
 }: {
+  imports = [
+    inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+  ];
+  
   services.displayManager.autoLogin = {
     user = username;
     enable = true;

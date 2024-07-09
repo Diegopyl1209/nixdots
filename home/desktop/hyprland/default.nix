59d3019b87cbaddb9941ba067c83b4a5deef6f38
Diego Peña y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2024-07-09 18:51:00 -0400

    fix hyprland config

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -4,12 +4,13 @@
   config,
   ...
 }: {
-  imports = [
-    ./config.nix
-    ./hyprlock.nix
-    ./hyprpaper.nix
-  ];
   config = lib.mkIf config.home-manager.hyprland.enable {
+    imports = [
+      ./config.nix
+      ./hyprlock.nix
+      ./hyprpaper.nix
+    ];
+
     home.packages = with pkgs; [
       xwaylandvideobridge
       libnotify
--- modules/display-manager.nix
+++ modules/display-manager.nix
@@ -14,7 +14,7 @@
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${pkgs.hyprland}/share/wayland-sessions";
+            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session";
             user = "greeter";
           };
         };

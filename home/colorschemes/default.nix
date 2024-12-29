d5f6cbc907d289592ccecf0cd42ad2832d14c887
Diego Peña y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2024-12-29 17:30:48 -0300

    fix server config

--- home/colorschemes/default.nix
+++ home/colorschemes/default.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  lib,
   pkgs,
   config,
   ...
@@ -8,44 +9,46 @@
     ./gruvbox.nix
   ];
 
-  stylix = {
-    enable = true;
-    autoEnable = false;
-    polarity = "dark";
-    image = ../desktop/wallpapers/${config.home-manager.wallpaper};
-    base16Scheme = "${config.scheme.outPath}";
-    targets = {
-      alacritty.enable = true;
-      kitty.enable = true;
-      gtk.enable = true;
-      helix.enable = true;
-      bat.enable = true;
-      gnome.enable = true;
+  config = lib.mkIf config.home-manager.graphical.enable {
+    stylix = {
+      enable = true;
+      autoEnable = false;
+      polarity = "dark";
+      image = ../desktop/wallpapers/${config.home-manager.wallpaper};
+      base16Scheme = "${config.scheme.outPath}";
+      targets = {
+        alacritty.enable = true;
+        kitty.enable = true;
+        gtk.enable = true;
+        helix.enable = true;
+        bat.enable = true;
+        gnome.enable = true;
+      };
     };
-  };
 
-  stylix.cursor = {
-    name = "phinger-cursors-dark";
-    package = pkgs.phinger-cursors;
-    size = 16;
-  };
+    stylix.cursor = {
+      name = "phinger-cursors-dark";
+      package = pkgs.phinger-cursors;
+      size = 16;
+    };
 
-  stylix.fonts = {
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans Condensed";
-    };
-    monospace = {
-      package = pkgs.nerd-fonts.jetbrains-mono;
-      name = "JetBrainsMono Nerd Font Mono";
-    };
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
+    stylix.fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Condensed";
+      };
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
     };
   };
 }
